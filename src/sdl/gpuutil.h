d3d9f72f3f190934eb2e2b044b97a68a88f2a72a
Marcus Welander <mwthinker@outlook.com>
2025-11-16 19:54:03 +0100

    Add buffer to util to simplify gpubuffer usage

--- src/sdl/gpuutil.h
+++ src/sdl/gpuutil.h
@@ -14,6 +14,71 @@ namespace sdl {
 	[[nodiscard]]
 	SDL_Rect blitToGpuTexture(SDL_GPUDevice* gpuDevice, SDL_GPUTexture* texture, sdl::ImageAtlas& imageAtlas, SDL_Surface* surface, int border);
 
+	class Buffer {
+	public:
+		template <typename T>
+		[[nodiscard]]
+		SDL_GPUBuffer* get(SDL_GPUDevice* gpuDevice, SDL_GPUBufferUsageFlags flag, std::span<const T> data) {
+			if (!buffer_.get() || bytes_ < data.size_bytes()) {
+				bytes_ = data.size_bytes();
+
+				SDL_GPUBufferCreateInfo vertexBufferInfo{
+					.usage = flag,
+					.size = static_cast<Uint32>(bytes_)
+				};
+				buffer_ = sdl::createGpuBuffer(gpuDevice, vertexBufferInfo);
+			}
+			return buffer_.get();
+		}
+		
+		[[nodiscard]]
+		SDL_GPUBuffer* get() {
+			return buffer_.get();
+		}
+
+		void reset() {
+			bytes_ = 0;
+			buffer_.reset();
+		}
+
+	private:
+		size_t bytes_ = 0;
+		sdl::GpuBuffer buffer_;
+	};
+
+	class TransferBuffer {
+	public:
+		template <typename T>
+		[[nodiscard]]
+		SDL_GPUTransferBuffer* get(SDL_GPUDevice* gpuDevice, std::span<const T> data, bool cycle = false) {
+			if (!transferBuffer_.get() || bytes_ < data.size_bytes()) {
+				bytes_ = data.size_bytes();
+
+				SDL_GPUTransferBufferCreateInfo transferInfo{
+					.usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
+					.size = static_cast<Uint32>(bytes_)
+				};
+				transferBuffer_ = sdl::createGpuTransferBuffer(gpuDevice, transferInfo);
+			}
+			sdl::mapGpuTransferBuffer(gpuDevice, transferBuffer_.get(), data, cycle);
+			return transferBuffer_.get();
+		}
+
+		[[nodiscard]]
+		SDL_GPUTransferBuffer* get() {
+			return transferBuffer_.get();
+		}
+
+		void reset() {
+			bytes_ = 0;
+			transferBuffer_.reset();
+		}
+
+	private:
+		size_t bytes_ = 0;
+		sdl::GpuTransferBuffer transferBuffer_;
+	};
+
 }
 
 #endif
